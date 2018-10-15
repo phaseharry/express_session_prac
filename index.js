b3102661a32b0457f72ad2569ff943a694eb9408
Jennifer Chen <jenniferchen@Jennifers-iMac.home>
2018-10-14 23:47:03 -0400

    practice session

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const session = require('express-session')
+const app = express()
+
+app.use(session({
+    secret: 'SunnyBeaches', //mandatory configuration ensures that sessions IDs are not predictable
+    resave: false,
+    saveUninitialized: true
+}))
+
+app.use((req, res, next) => {
+    console.log('SESSION: ', req.session)
+    next()
+})
+
+app.use((req, res, next) => {
+    if(!req.session.counter) req.session.counter = 0
+    console.log('counter', ++req.session.counter) //increment then log
+    next() //needed to continue through express middleware
+})
+
+app.get('/', (req, res, next) => {
+    res.send('Hello')
+})
+
+app.listen(8080, () => console.log('Listening at http://localhost:8080'))
